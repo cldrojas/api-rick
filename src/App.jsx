c21b5d4e64f971f81a4dad71b245ca0ff1d37251
Daniel Rojas <cld.rojasz@gmail.com>
2021-12-19 00:17:26 -0300

    add characters list from api

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ function App() {
     <div className="container">
       <h1>App Rick and Morty</h1>
       <Form setNombre={setNombre} />
+      <button
+        className="btn btn-success btn-block mt-2"
+        onClick={() => setNombre("")}
+      >
+        Reset
+      </button>
       <PintarDatos personaje={nombre} />
     </div>
   );
--- src/components/PintarDatos.jsx
+++ src/components/PintarDatos.jsx
@@ -1,5 +1,7 @@
 import Swal from "sweetalert2";
+
 import { useEffect, useState } from "react";
+import Personaje from "./Personaje";
 
 const PintarDatos = ({ personaje }) => {
   useEffect(() => {
@@ -7,28 +9,40 @@ const PintarDatos = ({ personaje }) => {
   }, [personaje]);
 
   const [personajes, setPersonajes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getData = async (nombre) => {
     const url = `https://rickandmortyapi.com/api/character/?name=${nombre}`;
     try {
+      setLoading(true);
       const res = await fetch(url);
 
-      if (!res.ok) {
+      if (!res.ok)
         return Swal.fire("Error", "No se encontró el personaje", "error");
-      }
 
       const data = await res.json();
       setPersonajes(data.results);
     } catch (error) {
       console.log(error);
     } finally {
+      setLoading(false);
     }
   };
 
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div className="row">
       {personajes.map((personaje) => (
-        <h3 key={personaje.id}>{personaje.name}</h3>
+        <Personaje key={personaje.id} data={personaje} />
       ))}
     </div>
   );
